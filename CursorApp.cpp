8dc5c5596e83507014ae5057b59306210fae5420
IsSomeoneElse <114873803+IsSomeoneElse@users.noreply.github.com>
2022-11-07 18:53:03 +0200

    Add files via upload

---
+++ CursorApp.cpp
@@ -0,0 +1,33 @@
+﻿#include "cursor.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    Cursor cursor;
+
+    Cursor curs1(20, 45);
+    Cursor curs2(24, 50, CursorType::Vertical);
+    Cursor curs3(54, 34, CursorType::Horizontal, 14);
+    cout << "Results with Init() method:" << endl;
+    cursor.display();
+    cout << endl;
+    cout << "Results with additional constructors:" << endl;
+    curs1.display();
+    curs2.display();
+    curs3.display();
+    cout << endl;
+
+    cout << "Results with operation overload:" << endl;
+    cursor++;
+    cursor.display();
+    cursor--;
+    cursor.display();
+
+    cursor.read();
+    cout << "Results with Read() method:" << endl;
+    cursor.display();
+
+    system("PAUSE");
+    return 0;
+}
